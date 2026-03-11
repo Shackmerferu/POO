30d6b75262d32d6a438f754538941c7c3fad39b5
Shackmerferu <Lucianocayssials@gmail.com>
2026-03-11 17:00:15 -0300

    cambio main y metodos

---
+++ Ej1/Main.java
@@ -0,0 +1,13 @@
+package Ej1;
+
+public class Main {
+    public static void main(String[] args){
+
+        VideoJuego Jueguito = new VideoJuego("carlos duty", 0);
+        VideoJuego Jueguito2 = new VideoJuego("el principe de los serpa", 500);
+        VideoJuego Jueguito3 = new VideoJuego("zendaya", 200);
+    
+       VideoJuego.getCantidadDeVideoJuegosCreados();
+    
+    }
+}
--- Ej1/VideoJuego.java
+++ Ej1/VideoJuego.java
@@ -1,37 +1,53 @@
 package Ej1;
 import java.time.LocalTime;
 
-public class VideoJuego{
-    private int cantidadDeVideoJuegosCreados;
+public class VideoJuego {
+
+    private static int cantidadDeVideoJuegosCreados = 0;
     private String nombre;
     private LocalTime fechaDeLanzamiento;
     private double precio;
 
-    public void jugar(){System.out.println("A jugar!!!");}
-    
+    public void jugar() {
+        System.out.println("A jugar!!!");
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     @Override
     public String toString() {
-        return "Name:" + nombre + "Cantidad:" + this.cantidadDeVideoJuegosCreados + "Lanzamiento:" + this.fechaDeLanzamiento + "Precio(ARS):" + this.precio;
+        return "Name:" + nombre + " Lanzamiento:" + this.fechaDeLanzamiento + " Precio(ARS):" + this.precio;
     }
-    //Es poliformismo estatico
-    public VideoJuego(double precio){
+    //Es polimorfismo estatico
+    public VideoJuego(String nombre , double precio) {
         this.precio = precio;
+        this.nombre = nombre;
+        this.fechaDeLanzamiento = LocalTime.now();
+        cantidadDeVideoJuegosCreados++;
     }
 
-    public VideoJuego(int precio){
+    public VideoJuego(String nombre , int precio) {
         this.precio = precio;
+        this.fechaDeLanzamiento = LocalTime.now();
+        cantidadDeVideoJuegosCreados++;
     }
-    
-    public VideoJuego(String precio){
+
+    public VideoJuego(String nombre , String precio) {
         this.precio = Double.parseDouble(precio);
+        this.fechaDeLanzamiento = LocalTime.now();
+        cantidadDeVideoJuegosCreados++;
     }
+
+    public static int getCantidadDeVideoJuegosCreados() {
+        return cantidadDeVideoJuegosCreados;
+    }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setFechaDeLanzamiento(LocalTime fechaDeLanzamiento) {
         this.fechaDeLanzamiento = fechaDeLanzamiento;
     }
@@ -39,7 +55,4 @@ public class VideoJuego{
     public LocalTime getFechaDeLanzamiento() {
         return fechaDeLanzamiento;
     }
-
 }
-
-//comentario prueba
