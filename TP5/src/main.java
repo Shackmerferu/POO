011c58e73bf1d27bb8b5d3732240f2b6f39e7b4a
HeroedelGin <antonio42489167@gmail.com>
2026-05-03 21:29:00 -0300

    Loco, cuando termina este sufrimiento... Tp5, E1,E2 FINALIZADOS

--- TP5/src/main.java
+++ TP5/src/main.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String[] args) {
-
+    /* 
         Pong pong = new Pong();
         PiedraPapelTijera ppt = new PiedraPapelTijera();
         Dados dados = new Dados();
@@ -18,6 +20,31 @@ public class Main{
         System.out.println("----- Dados -----");
         System.out.println(dados.getNombre());
         System.out.println(dados);
-        dados.jugar();
+        dados.jugar();*/
+         ArrayList<Multiplicable> lista = new ArrayList<>();
+
+        // Crear objetos
+        NumeroEntero num = new NumeroEntero(5);
+
+        int[] vec = {1, 2, 3};
+        MiVector vector = new MiVector(vec);
+
+        int[][] mat = {
+            {1, 2},
+            {3, 4}
+        };
+        MiMatriz matriz = new MiMatriz(mat);
+
+        // Agregar a la lista
+        lista.add(num);
+        lista.add(vector);
+        lista.add(matriz);
+
+        // Recorrer y aplicar operaciones
+        for (Multiplicable obj : lista) {
+            obj.multiplicar(2);
+            obj.mostrarResultado();
+            System.out.println("-----");
+        }
     }
 }
