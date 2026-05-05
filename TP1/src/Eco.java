c14711ff24a80c5b8e1a882b3d2d1c76a10d5398
Shackmerferu <lucianocayssials@gmail.com>
2026-05-05 13:25:13 -0300

    rama unica proyecto poo (se eliminan carpeta innecesarias)

--- TP1/src/Eco.java
+++
@@ -1,9 +0,0 @@
-public class Eco {
-    public static void main(String[] args) {
-
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-        System.out.println("cantidad de argunmentos " + args.length);
-    }
-}
--- TP5/README.md
+++
@@ -1,1 +0,0 @@
-Tp5, Prueba 1,2,3 Que miras gay.
