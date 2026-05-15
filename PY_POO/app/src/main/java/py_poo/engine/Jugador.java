60821e7bf8a6af692a29deceb3f26d9d169b5f20
Shackmerferu <lucianocayssials@gmail.com>
2026-05-15 14:06:29 -0300

    quitar parametrizado sin uso

--- PY_POO/app/src/main/java/py_poo/engine/Jugador.java
+++ PY_POO/app/src/main/java/py_poo/engine/Jugador.java
@@ -2,7 +2,7 @@ package py_poo.engine;
 
 public class Jugador {
     private String Nombre;
-    public Jugador(String Nombre){
+    public Jugador(){
         this.Nombre="Player";
     }
     public void setNombre(String Nombre){
