e3fea6e4de47d552be12fa8e2e409857d283e363
Shackmerferu <lucianocayssials@gmail.com>
2026-05-25 14:17:31 -0300

    Cambios de nivel

--- PY_POO/app/src/main/java/py_poo/loderunner/Nivel1.java
+++ PY_POO/app/src/main/java/py_poo/loderunner/Nivel1.java
@@ -4,19 +4,19 @@ public class Nivel1  extends  Nivel{
     String[] nivel1 = {
     "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     "X            O           O X",
-    "X   #######      #######   X",
+    "X   ######E######E#####E###X",
     "X   E     E  B   E     E   X",
     "X   E  O  EBBBBB E  O  E   X",
-    "X   E#####E     E#####E   GX",
-    "X        E   O   E         X",
-    "XBBBBBB  E#######E  BBBBBBX",
-    "X     E  E       E  E      X",
-    "X O   E####### ####E   O   X",
+    "X   E#### E      E#####E   X",
+    "X         E  O   E        GX",
+    "XBBBBBB   E######E  EBBBBBBX",
+    "X     E   E      E  E      X",
+    "X O   E#############E   O  X",
     "X#####E             E######X",
     "X     E   BBBBBBB   E      X",
     "X  O  E      O      E  O   X",
     "X#####E#############E######X",
-    "XR         A               X",
+    "X     E  R                 X",
     "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
 }
--- PY_POO/app/src/main/java/py_poo/loderunner/Nivel2.java
+++ PY_POO/app/src/main/java/py_poo/loderunner/Nivel2.java
@@ -3,7 +3,7 @@ package py_poo.loderunner;
 public class Nivel2 {
     String[] nivel2 = {
     "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "X O      #######      O    X",
+    "X O      E#####E      O    X",
     "X####### E     E #######   X",
     "X      E E  O  E E       O X",
     "X BBBB E E#####E E BBBBBBB X",
@@ -14,9 +14,9 @@ public class Nivel2 {
     "X      E       E E         X",
     "X######E BBBBB E E BBBBBBB X",
     "X      E   O   E E         X",
-    "X BBBB E#######E E#########X",
-    "X      E       E E         X",
-    "XR         A              X",
+    "X BBBB E######## E#########X",
+    "X      E         E         X",
+    "XR     E    A    E         X",
     "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 };
 }
